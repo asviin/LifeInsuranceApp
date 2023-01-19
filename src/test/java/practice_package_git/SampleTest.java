5752e51cac5680ada3d202c045870330f7648d27
hp <hp@DESKTOP-9BB5NPN>
2023-01-19 13:06:26 +0530

    New package created.

---
+++ src/test/java/practice_package_git/SampleTest.java
@@ -0,0 +1,14 @@
+package practice_package_git;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void sample() {
+		System.out.println("----testscript1---");
+	}
+	
+	
+
+}
