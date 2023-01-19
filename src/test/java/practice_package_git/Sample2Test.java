5594ef094bc228ad5a70baa8815e69b365e36e44
hp <hp@DESKTOP-9BB5NPN>
2023-01-19 13:12:36 +0530

    New branch is created.

---
+++ src/test/java/practice_package_git/Sample2Test.java
@@ -0,0 +1,12 @@
+package practice_package_git;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test {
+	
+	@Test
+	public void sample2() {
+		System.out.println("-----------testscript2--------");
+	}
+
+}
